33f6bb26758572626cd2d3fd219fae89baeb4ab9
Dipak1121 <91940812+Dipak1121@users.noreply.github.com>
2023-10-30 20:33:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,7 +4,12 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
-  
+	let ans = 0;
+	let prices = document.querySelectorAll(".price");
+	for ( let i=0; i < prices.length; i++){
+		ans = ans + parseInt(prices[i].innerText);
+	}
+  return ans;
 };
 
 getSumBtn.addEventListener("click", getSum);
