0793f71580179d340144c4aededc8909d8974a65
Dipak1121 <91940812+Dipak1121@users.noreply.github.com>
2023-10-30 20:48:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+const table = document.querySelector("table");
 const getSumBtn = document.createElement("button");
 getSumBtn.append("Get Total Price");
 document.body.appendChild(getSumBtn);
@@ -9,7 +10,15 @@ const getSum = () => {
 	for ( let i=0; i < prices.length; i++){
 		ans = ans + parseInt(prices[i].innerText);
 	}
-  return ans;
+  let td1 = document.createElement("td");
+	td1.innerText = "Total Price";
+	let td2 = document.createElement("td");
+	td2.id = "ans";
+	td2.innerText = `${ans}`;
+	let tr = document.createElement("tr");
+	tr.appendChild(td1);
+	tr.appendChild(td2);
+	table.appendChild(tr);
 };
 
 getSumBtn.addEventListener("click", getSum);
